4b8e9edec5a898f8a093144b3b5e437bc8f169bd
Ric <ricjohn.genoguin@gmail.com>
2024-04-02 12:26:56 +1300

    feat: implement --file command

--- user_upload.php
+++ user_upload.php
@@ -1,9 +1,20 @@
 <?php
-echo print_r($argv);
+//echo print_r($argv);
 
 if (isset($argv)) {
     if (in_array("--help", $argv)) {
         echo "help!";
+        return;
+    }
+
+    if (in_array("--file", $argv)) {
+        $file = NULL;
+        foreach ($argv as $arg) {
+            if (str_contains($arg, "csv")) {
+                $file = $arg;
+            }
+        }
+        echo $file ? "Importing {$file}.." : "Incorrect usage. (e.g: --file users.csv)";
         return;
     }
 
