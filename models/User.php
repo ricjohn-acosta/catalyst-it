1613f61c964ec53bbc2f87ca1ce03aa9d1150c26
Ric <ricjohn.genoguin@gmail.com>
2024-04-03 14:49:09 +1300

    refactor: add basic sanitization, validation and use STDOUT for logging errors

--- models/User.php
+++ models/User.php
@@ -13,21 +13,43 @@ class User {
 
     public function add($name, $surname, $email)
     {
+        if (!$this->validate_email($email)) {
+            fwrite(STDOUT, "\nFailed to upload user {$surname}. Invalid email - {$email}.\n");
+            return;
+        }
+
         try {
             $result = $this->pdo->exec(
                 '
                 USE catalyst;
                 INSERT INTO users (name, surname, email)
-                VALUES ( "'.$name.'", "'.$surname.'", "'.$email.'");
+                VALUES ( "'.$this->sanitize_name($name).'", "'.$this->sanitize_name($surname).'", "'.$this->sanitize_email($email).'");
             '
             );
 
             if ($result === 0) {
-                echo "Successfully uploaded user!\n";
+                echo "\nSuccessfully uploaded user!\n";
             }
 
         } catch (PDOException $exception) {
-            echo "\n{$exception->getMessage()}\n\n";
+            fwrite(STDOUT, "\n{$exception->getMessage()}\n");
         }
+    }
+
+    private function validate_email($email)
+    {
+        $pattern = "/^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/";
+        return preg_match($pattern, $email) === 1 && filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    private function sanitize_name($name)
+    {
+        $lowercased_name = strtolower($name);
+        return ucfirst($lowercased_name);
+    }
+
+    private function sanitize_email($email)
+    {
+        return strtolower($email);
     }
 }
