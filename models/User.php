19e9fbb7c4e4c8b0152f18feb7e9d5750c36ec56
Ric <ricjohn.genoguin@gmail.com>
2024-04-03 17:31:37 +1300

    refactor: add types and fix formatting

--- models/User.php
+++ models/User.php
@@ -14,18 +14,21 @@ class User {
     public function add($name, $surname, $email, $dry_run): void
     {
         if (!$this->validate_email($email)) {
-            fwrite(STDOUT, "\nFailed to upload user {$surname}. Invalid email - {$email}.\n");
+            fwrite(STDOUT, "\nFailed to upload user {$name}. Invalid email - {$email}.\n");
             return;
         }
 
         try {
-            $result = $this->pdo->exec(
+
+            $query_result = NULL;
+            if (!$dry_run) {
+                $query_result = $this->pdo->exec('
+                    USE catalyst;
+                    INSERT INTO users (name, surname, email)
+                    VALUES ( "'.$this->sanitize_name($name).'", "'.$this->sanitize_name($surname).'", "'.$this->sanitize_email($email).'");
                 '
-                USE catalyst;
-                INSERT INTO users (name, surname, email)
-                VALUES ( "'.$this->sanitize_name($name).'", "'.$this->sanitize_name($surname).'", "'.$this->sanitize_email($email).'");
-            '
-            );
+                );
+            }
 
             if ($query_result === 0 || $dry_run) {
                 echo "\nSuccessfully uploaded user {$name}\n";
@@ -36,19 +39,20 @@ class User {
         }
     }
 
-    private function validate_email($email)
+    private function validate_email($email): bool
     {
         $pattern = "/^[\w.-]+@([\w-]+\.)+[\w-]{2,4}$/";
+        // Here we make sure that the email is in a legal format as well as if it is a valid email.
         return preg_match($pattern, $email) === 1 && filter_var($email, FILTER_VALIDATE_EMAIL);
     }
 
-    private function sanitize_name($name)
+    private function sanitize_name($name): string
     {
         $lowercased_name = strtolower($name);
         return ucfirst($lowercased_name);
     }
 
-    private function sanitize_email($email)
+    private function sanitize_email($email): string
     {
         return strtolower($email);
     }
