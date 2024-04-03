5b7db5bd1738d448b42f0ce66876de71737f205e
Ric <ricjohn.genoguin@gmail.com>
2024-04-03 14:59:38 +1300

    refactor: add help menu

--- user_upload.php
+++ user_upload.php
@@ -6,7 +6,23 @@ global $db;
 
 if (isset($argv)) {
     if (in_array("--help", $argv)) {
-        echo "help!";
+        echo "
+         ########################################################################################
+         
+            Welcome to CatalystCRM! Your one stop shop for your user uploading needs!
+            
+            You may use the following flags:
+            
+            --file [csv file name]  = upload users into the database
+            --create_table          = creates a users table in mysql  
+            --dry_run               = to be used in conjunction with --file flag to run the application without modifying the database
+            -u                      = display mysql username
+            -p                      = display mysql password
+            -h                      = display mysql host
+            --help                  = display help menu
+         
+         ########################################################################################
+        ";
         return;
     }
 
